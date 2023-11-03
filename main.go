ad7e12b934b4dcc545476939ddda687b73a93ec1
Matias <matisk.90@gmail.com>
2023-11-03 13:25:23 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/msack22/go/variables"
 )
 
 func main() {
-	variables.MuestroEntero()
+	variables.RestoVariables()
 
+	estado, text := variables.ConviertoaTexto(10)
+	fmt.Println(estado)
+	fmt.Println(text)
 }
